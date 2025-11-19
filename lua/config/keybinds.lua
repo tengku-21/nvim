4a217dbe689f2216c240a00538e5718555b0636b
tengku-faizzuan <faiztg21@gmail.com>
2025-11-19 18:30:03 +0800

    disable tree use def netrw

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -13,3 +13,21 @@ vim.keymap.set("n", "gr", vim.lsp.buf.references)       -- Find references
 vim.keymap.set("n", "K", vim.lsp.buf.hover)             -- Hover info
 vim.keymap.set("n", "E", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 
+-- Toggle netrw (:Ex)
+vim.keymap.set("n", "<leader>e", function()
+    if vim.bo.filetype == "netrw" then
+        -- if netrw is open, close it safely
+        vim.cmd("bd")
+    else
+        -- otherwise, open netrw
+        vim.cmd("Ex")
+    end
+end, { desc = "Toggle Explorer" })
+
+-- Map <leader>cd to change to current file's directory
+vim.keymap.set("n", "<leader>cd", ":cd %:p:h<CR>:pwd<CR>", { desc = "CD to current file dir" })
+
+vim.keymap.set("n", "<leader>E", function()
+    vim.cmd("Vexplore")  -- opens netrw vertically
+end)
+
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,48 +1,48 @@
-return{
-    "nvim-tree/nvim-tree.lua",
-    dependencies= {
-        "nvim-tree/nvim-web-devicons",
-    },
-config = function()
-    local api = require("nvim-tree.api")
-
-    require("nvim-tree").setup({
-        view = { side = "left" },
-        sync_root_with_cwd = true,
-    })
-
-    -- Global keymap to toggle tree
-    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
-
-    -- Keymap inside nvim-tree only:
-    
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "NvimTree",
-    callback = function()
-        vim.keymap.set("n", "<leader><CR>", function()
-            local api = require("nvim-tree.api")
-
-            -- Change tree root
-            api.tree.change_root_to_node()
-
-            -- Get node
-            local node = api.tree.get_node_under_cursor()
-            if not node then return end
-
-            -- Directory to switch to
-            local path = node.absolute_path
-            if node.type == "file" then
-                path = vim.fn.fnamemodify(path, ":h")
-            end
-
-            -- 🔥 Make the cwd permanent
-            vim.api.nvim_set_current_dir(path)
-            print("CWD set to: " .. path)
-        end, { buffer = true, noremap = true, silent = true })
-    end
-})
-
-end
-
+-- return{
+--     "nvim-tree/nvim-tree.lua",
+--     dependencies= {
+--         "nvim-tree/nvim-web-devicons",
+--     },
+-- config = function()
+--     local api = require("nvim-tree.api")
+--
+--     require("nvim-tree").setup({
+--         view = { side = "left" },
+--         sync_root_with_cwd = true,
+--     })
+--
+--     -- Global keymap to toggle tree
+--     vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+--
+--     -- Keymap inside nvim-tree only:
+--
+--
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "NvimTree",
+--     callback = function()
+--         vim.keymap.set("n", "<leader><CR>", function()
+--             local api = require("nvim-tree.api")
+--
+--             -- Change tree root
+--             api.tree.change_root_to_node()
+--
+--             -- Get node
+--             local node = api.tree.get_node_under_cursor()
+--             if not node then return end
+--
+--             -- Directory to switch to
+--             local path = node.absolute_path
+--             if node.type == "file" then
+--                 path = vim.fn.fnamemodify(path, ":h")
+--             end
+--
+--             -- 🔥 Make the cwd permanent
+--             vim.api.nvim_set_current_dir(path)
+--             print("CWD set to: " .. path)
+--         end, { buffer = true, noremap = true, silent = true })
+--     end
+-- })
+--
+-- end
+--
 }
